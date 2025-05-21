88e66c4d826b0d9a51a652484b1c2bdbeaf62bf6
Luca Bartelsen <luca@macbookair>
2025-05-21 10:27:25 +0200

    adjutest core time

--- app/models/core.py
+++ app/models/core.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timezone
 from typing import Any, List, Optional, Dict
 from sqlmodel import DateTime, Field, Relationship, SQLModel
 from sqlalchemy import Column, text
@@ -9,5 +9,5 @@ from pydantic import validator
 # Base model for all SQLModel models
 class BaseModel(SQLModel):
     id: Optional[int] = Field(default=None, primary_key=True)
-    created_at: datetime = Field(default_factory=datetime.timezone.now, sa_column=Column(DateTime, default=func.now(), nullable=False))
-    updated_at: datetime = Field(default_factory=datetime.timezone.now,sa_column=Column(DateTime, default=func.now(), onupdate=func.now(), nullable=False))
+    created_at: datetime = Field(default_factory=datetime.now(timezone.utc), sa_column=Column(DateTime, default=func.now(), nullable=False))
+    updated_at: datetime = Field(default_factory=datetime.now(timezone.utc),sa_column=Column(DateTime, default=func.now(), onupdate=func.now(), nullable=False))
